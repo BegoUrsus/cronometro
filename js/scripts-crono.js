9743266b7d48fb31b5640d157ddce4ef9e42d41e
BegoUrsus <layira2002@gmail.com>
2016-05-07 23:46:06 +0200

    fixed play sound issues

--- js/scripts-crono.js
+++ js/scripts-crono.js
@@ -464,7 +464,6 @@ $(function() {
 		// Cada segundo
 		if (milesimas % 1000 === 0) {
 			var segundos = milesimas / 1000;
-			sonando = true;
 			if (segundos > 0 && sonando) {
 				audiotick.muted = false;
 				audiotick.play();
